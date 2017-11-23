3ceaedc28373e956c9e83e0347f67ec0b9b67245
Timmy FURET <tifuret@e2r1p14.42.fr>
2017-11-23 15:48:05 +0100

    [clean] deleted cbaillat's code

--- srcs/input.c
+++ srcs/input.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 18:46:37 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/11/21 19:11:35 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/11/23 15:47:31 by tifuret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,3 @@
 #include "./lib/libft.h"
 
 #define RAW_TETRIMINOS 21
-
-t_tetri	*ft_readtetriminos(int file_des)
-{
-	t_tetri	*tetriminos;
-	char	tetriminos_raw[RAW_TETRIMINOS + 1];
-	uint64_t	index;
-
-	while (read(file_des, tetriminos_raw, 21))
-	{
-		;
-	}
-	return (tetriminos);
-}

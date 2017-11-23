a300c4490bf70cd337703ae33c4d48433d8a9f49
Camille Baillat <camille.baillat@protonmail.com>
2017-11-23 15:45:05 +0100

    [dev] added list pointers to the tetri struct

--- srcs/fillit.h
+++ srcs/fillit.h
@@ -6,7 +6,7 @@
 /*   By: cbaillat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 15:10:55 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/11/23 15:18:53 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/11/23 15:38:00 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 #define FAILURE	1
 
 #include "unistd.h"
-
-//debug
-#include <stdio.h>
+#include "stdlib.h"
 
 typedef struct	s_tetri
 {
-	char	*tetriminos;
-	char	rank;
-	int		test;
+	char			*tetriminos;
+	char			rank;
+	struct s_tetri	*next;
+	struct s_tetri	*prev;
 }				t_tetri;

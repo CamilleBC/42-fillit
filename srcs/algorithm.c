fe840082a4b36d5eaa268ed6117306d6107eb3ff
Camille Baillat <camille.baillat@protonmail.com>
2017-11-24 11:32:30 +0100

    [wip] algorithm

--- srcs/algorithm.c
+++ srcs/algorithm.c
@@ -6,19 +6,40 @@
 /*   By: cbaillat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 14:39:35 by cbaillat          #+#    #+#             */
-/*   Updated: 2017/11/23 19:21:53 by cbaillat         ###   ########.fr       */
+/*   Updated: 2017/11/24 11:31:04 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static uint32_t	ft_get_min_size(t_tetri *tetriminos)
+static t_bool	get_solutionsize(t_tetri *tetri, uint32_t *min_size,
+		uint32_t *max_size)
 {
-	uint32_t	size;
+	uint32_t	elements;
+	uint32_t	max_width;
+	uint32_t	max_length;
 
-	if (tetriminos == NULL)
+	elements = 0;
+	*min_size = 0;
+	*max_size = 0;
+	max_length = 0;
+	max_width = 0;
+	if (tetri == NULL)
 		return (FAILURE);
-	while (tetriminos->next != NULL)
-		++size;
+	while (tetri->next != NULL)
+	{
+		++elements;
+		max_length += tetri->length;
+		max_width += tetri->width;
+	}
+	*max_size = (max_length > max_width) ? max_length : max_width;
+	*min_size = elements << 2;
+	return (SUCCESS);
+}
 
+static t_bool	place_tetri(t_tetri *tetri, t_map *map, uint32_t location)
+{
+	uint32_t i;
+
+	if (tetri[i] 
 }
